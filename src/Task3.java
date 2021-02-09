2e4393c294176cbee7c54351c6dcb380ac723de7
Alex Nasevich <nasevichbsu@gmail.com>
2021-02-09 16:01:57 +0300

    add Task3 with comment

---
+++ src/Task3.java
@@ -0,0 +1,13 @@
+/*
+Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int j : array) {
+            if (j % 3 == 0)
+                System.out.println(j);
+        }
+    }
+}
