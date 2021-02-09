a0f489f30c146145c087814245d0a51a0782c428
Alex Nasevich <nasevichbsu@gmail.com>
2021-02-09 15:47:03 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+Составить алгоритм: если введенное число больше 7, то вывести “Привет”
+*/
+
+public class Task1 {
+    public static void printHello() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number > 7)
+            System.out.println("Привет");
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введите целое число");
+        printHello();
+    }
+}
